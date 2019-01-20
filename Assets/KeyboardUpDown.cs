980ccedace39a6a5c7f5c38c22a24e87019dffc0
Scott Black <scott.black@usu.edu>
2019-01-20 05:38:51 -0700

    add intesecting line highlighting and rod movement

---
+++ Assets/KeyboardUpDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class KeyboardUpDown : MonoBehaviour {
+
+    public float yLimit = 150;
+    public float speed = 1f;
+
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > -yLimit)
+        {
+            rb.AddForce(Vector3.down * speed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < yLimit)
+        {
+            rb.AddForce(Vector3.up * speed);
+        }
+    }
+}
---
+++ Assets/LightUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightUp : MonoBehaviour {
+
+    LineRenderer line;
+    Color startColor;
+    Color endColor;
+
+    public Color highlightStartColor;
+    public Color highlightEndColor;
+
+	void Start () {
+        line = GetComponentInParent<LineRenderer>();
+        startColor = line.startColor;
+        endColor = line.endColor;
+    }
+	
+	void OnTriggerEnter(Collider col)
+    {
+        line.startColor = highlightStartColor;
+        line.endColor = highlightEndColor;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        line.startColor = startColor;
+        line.endColor = endColor;
+    }
+}
